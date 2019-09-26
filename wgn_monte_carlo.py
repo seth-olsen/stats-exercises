b743553a49cad4334ecac0c86aeccf3b91d32014
Seth Olsen <setholsenr@gmail.com>
2019-09-26 15:00:50 -0400

    almost finished with problem 1

--- wgn_monte_carlo.py
+++ wgn_monte_carlo.py
@@ -12,16 +12,14 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-from csv import reader, writer, QUOTE_NONNUMERIC
-from time import time
-
-#sample_size = 10**6
-#tau = 100
-#alpha = 1 / float(sample_size**2)
-#eta = np.sqrt(tau * np.log(1 / alpha))
+from csv import writer
+#from time import time
+import sys
 
 def write_data_txt(data, name='data'):
-    filename = name + '.txt'
+    filename = name
+    if filename[-4:] != '.txt':
+        filename += '.txt'
     f = open(filename, 'w')
     for entry in data:
         f.write(str(entry) + '\n')
@@ -29,7 +27,9 @@ def write_data_txt(data, name='data'):
     return
 
 def write_data_csv(data, name='data'):
-    filename = name + '.csv'
+    filename = name
+    if filename[-4:] != '.csv':
+        filename += '.csv'
     f = open(filename, 'w', newline='')
     wr = writer(f)
     for row in data:
@@ -37,28 +37,25 @@ def write_data_csv(data, name='data'):
     f.close()
     return
 
-def read_data_txt(name):
-    filename = name
-    if filename[-4:] != '.txt':
-        filename += '.txt'
-    f = open(filename)
-    data = [line.rstrip() for line in f]
-    f.close()
-    return np.array(map(float, data))
+def read_zstat_files(start_ind, max_ind, name='mc_zstats', fext='.csv'):
+    delim = ','
+    if fext != '.csv':
+        delim = '\n'
+    arrs = []
+    for i in range(start_ind, max_ind):
+        filename = name + str(i) + fext
+        arrs.append(np.genfromtxt(filename, delimiter=delim))
+    return np.concatenate(arrs)
 
-def read_data_csv(name):
-    filename = name
-    if filename[-4:] != '.csv':
-        filename += '.csv'
-    f = open(filename, newline='')
-    data = reader(f, quoting=QUOTE_NONNUMERIC)
-    f.close()
-    return np.array(data)
+def combine_zstat_files(start_ind, max_ind, name='mc_zstats', fext='.csv'):
+    outname = name + '_' + str(start_ind) + '_' + str(max_ind)
+    write_data_csv(read_zstat_files(start_ind, max_ind, \
+                                    name=name, fext=fext), name=outname)
+    return
 
-def plot_hist(dataname, figname='histogram', bin_edges=[]):
-    data = read_data_csv(dataname)
+def plot_hist(data, figname='mc_hist', bin_edges=[]):
     fig = plt.figure()
-    n, bins, patches = plt.hist(x=data, bins='auto', color='#0504aa',
+    n, bins, patches = plt.hist(data, bins='auto', color='#0504aa',
                                 alpha=0.7, rwidth=0.85)
     plt.grid(axis='y', alpha=0.75)
     plt.xlabel('Max Text Statistic')
@@ -68,7 +65,15 @@ def plot_hist(dataname, figname='histogram', bin_edges=[]):
         figname += '.png'
     fig.savefig(figname)
     return
-    
+
+def plot_hist_file(dataname, figname='mc_hist', bin_edges=[]):
+    if dataname[-4:] != '.csv':
+        dataname += '.csv'
+    data = np.genfromtxt(dataname, delimiter=',')
+    plot_hist(data, figname=figname, bin_edges=bin_edges)
+    return
+
+
 
 def rect_pulse(arr_size, pulse_end, pulse_len):
     if arr_size < 1:
@@ -92,34 +97,38 @@ def complex_wgn(arr_size, mean=0, var=1):
     return re_part + 1j*im_part
 
 
-def mc_noise_zstats(arr_size, pulse_len, ntrials=0):
-    if ntrials == 0:
-        ntrials = arr_size
+def mc_wgn_zstats(arr_size, pulse_len, ntrials=1):
     trial = 0
-    z_stats = []
+    z_stats = np.zeros(ntrials)
     s_hat = rect_pulse(arr_size, arr_size, pulse_len)
     ft_s_hat = np.fft.fft(s_hat)
-    start_time = time()
+    #start_time = time()
     while trial < ntrials:
         data = complex_wgn(arr_size)
         ft_data = np.fft.fft(data)
         ft_z = ft_data * ft_s_hat
         z = np.fft.ifft(ft_z)
-        z_stats.append(max(np.absolute(z)))
+        z_stats[trial] = max(np.absolute(z))
         trial += 1
-    print('completed in', time() - start_time, 'seconds')
+    #print('completed in', time() - start_time, 'seconds')
     return z_stats
 
 
-def main(name='mc_zstats', sample_size=1000000, tau=100, trial_factor=1):
-    ntrials = sample_size * trial_factor
-    data = mc_noise_zstats(sample_size, tau, ntrials=ntrials)
-    write_data_csv(data, name=name)
+def main(outname, sample_size=1000000, tau=100, ntrials=100000):
+    data = mc_wgn_zstats(sample_size, tau, ntrials=ntrials)
+    write_data_csv(data, name=outname)
     return 0
 
-#main(name='mc_zstats_test', sample_size=1000, tau=10)
+#fname = 'mc_zstats'
+#if len(sys.argv) > 1:
+#    fname += sys.argv[1]
 
-print(mc_noise_zstats(10, 2, ntrials=3))
-    
-    
+#main(fname)
+
 #plot_hist('mc_zstats_test.csv', figname='hist-test')
+
+
+
+
+
+
