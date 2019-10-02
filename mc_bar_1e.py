e338c325d24235188bfcaea0d8fe3757261131f5
Seth Olsen <setholsenr@gmail.com>
2019-10-02 15:10:57 -0400

    lots of progress on jupyter notebook

--- mc_bar_1e.py
+++ mc_bar_1e.py
@@ -28,21 +28,17 @@ for i in range(len(sys.argv) - 1):
         outname_threshold = sys.argv[i+1]
 
 ft_signal = np.fft.fft(np.concatenate([np.zeros(N-tau), np.ones(tau)]))
-#max_stats = np.zeros(nsamples)
-#integrated_stats = np.zeros(nsamples)
+max_stats = np.zeros(nsamples)
 #all_stats = np.zeros((nsamples, N))
 for sample in range(nsamples):
-    #stat = abs2(np.fft.ifft(ft_signal * \
-                            #np.fft.fft(np.random.normal(0, 1, N) + 1j*np.random.normal(0, 1, N))))
-    #max_stats[sample] = max(stat)
-    #integrated_stats[sample] = np.sum(stat) / float(N)
+    stat = abs2(np.fft.ifft(ft_signal * \
+                            np.fft.fft(np.random.normal(0, 1, N) + 1j*np.random.normal(0, 1, N))))
+    max_stats[sample] = max(stat)
     #all_stats[sample] = abs2(np.fft.ifft(ft_signal * \
                                          #np.fft.fft(np.random.normal(0, 1, N) + \
                                                     #1j*np.random.normal(0, 1, N))))
 
-#np.savetxt(outname_stats, all_stats, delimiter=',')
-
-np.savetxt(outname_stats, all_stats, delimiter=',')
+np.savetxt(outname_stats, max_stats, delimiter=',')
 
 #threshold = np.quantile(max_stats, 1 - alpha)
 
